e0214f4924a868c3eb467864633b5dd8e7b5ce39
SetLordOfChaos <setlordchaos@gmail.com>
2026-04-02 22:20:49 -0700

    Update flake.nix to make username lowercase

--- nix/flake.nix
+++ nix/flake.nix
@@ -13,7 +13,7 @@
   outputs = { nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
-      username = "Set";
+      username = "set";
 
       pkgs = import nixpkgs {
         inherit system;
