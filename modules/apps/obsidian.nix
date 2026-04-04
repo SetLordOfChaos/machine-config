519fd7dba034bc4257ba12976c492c329d67d47e
Seth Najmon <sepana7@gmail.com>
2026-04-04 14:26:41 -0700

    correcting obsidian nix format

--- modules/apps/obsidian.nix
+++ modules/apps/obsidian.nix
@@ -1,11 +1,14 @@
 { pkgs, ... }:
 
 let
+  version = "1.12.7";
+
   obsidian = pkgs.appimageTools.wrapType2 {
-    name = "obsidian";
+    pname = "obsidian";
+    inherit version;
 
     src = pkgs.fetchurl {
-      url = "https://github.com/obsidianmd/obsidian-releases/releases/download/v1.12.7/Obsidian-1.12.7.AppImage";
+      url = "https://github.com/obsidianmd/obsidian-releases/releases/download/v${version}/Obsidian-${version}.AppImage";
       sha256 = "sha256:f6d8b96fe685a8632c819cc093a248ace0f6bab410f44a6c929a2611b1ebb17c";
     };
   };
